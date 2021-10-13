7a8fbc6c2675d9bcca3a9b8d7e52e3e7d36e5958
TheDevMK <thedevmk@gmail.com>
2021-10-13 14:15:53 +0530

    Initial commit

--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.java
@@ -11,6 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
